b4185e83e57182eb5f8044e50c318ff551924e75
lucosanta <luca.santonastasi@gmail.com>
2017-11-12 01:54:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,4 @@
-'''
-from Agent import Agent
-import logging
-from Enumerators import DebugLevel,Mode
-from LoggerConfigurator import configure_log
 
-
-import tensorflow as tf
-#Parameters
-run = 1
-debug=DebugLevel.VERBOSE
-url = 'http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'
-dataset_directory = './dataset'
-keep_prob=0.8
-image_shape = (32,32,3)
-
-#Configuration of the logger
-logger = configure_log('lsantonastasi - cifar-10', debug=DebugLevel.VERBOSE)
-
-a = Agent(log=logger, mode = Mode.TRAIN)
-
-#downloading dataset from Toronto!
-#a.download_dataset(url=url,dest_directory=dataset_directory)
-
-#convert to TFRecord
-a.convert_to_tf_record()
-
-
-
-
-# Model
-#a.build_network(dropout_prob=dropout_prob,image_shape = image_shape)
-
-#image_batch, label_batch =a.input(data_dir='./dataset/cifar-10-batches-py',subset='train',use_distortion=True,batch_size=2000)
-
-#logger.debug(str(image_batch[0].shape))
-#logger.debug(str(label_batch[0].shape))
-
-a.train(keep_prob=keep_prob,image_shape = image_shape)
-'''
 from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
